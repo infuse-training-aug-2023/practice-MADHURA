37541067c3f585b4440f481c6bafd29a61871e3d
madhura0404 <madhura.patil54@gmail.com>
2023-08-28 04:26:36 -0700

    changed 6.rb

--- selenium/6.rb
+++ selenium/6.rb
@@ -1,13 +1,21 @@
 require 'selenium-webdriver'
 
-Selenium::WebDriver::Chrome::Service.driver_path = 'C:\Users\MADHURA\Desktop\Git training\selenium\drivers\chromedriver.exe'
+Selenium::WebDriver::Chrome::Service.driver_path = 'C:\Users\madhura.patil\Desktop\Git training\selenium\drivers\chromedriver.exe'
 driver= Selenium::WebDriver.for :chrome
 
 driver.get "https://letcode.in/forms"
 
 dropdown = driver.find_element(:xpath, "/html/body/app-root/app-forms/section[1]/div/div/div[1]/div/div/form/div[5]/div[2]/div/div/div/select")
 
-puts dropdown.text
+select = Selenium::WebDriver::Support::Select.new(dropdown)
+
+# puts dropdown.text
+
+options = select.options
+
+options.each do |option|
+  puts option.text
+end
 
 dropdown.click 
   
